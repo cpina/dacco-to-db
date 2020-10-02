57f4a9bae47f0bda9e9b553271b1ae6b952e4e58
Carles Pina i Estany <carles@pina.cat>
2020-10-02 10:48:26 +0100

    Add words into the sqlite file

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import xml.dom.minidom
+import xml.etree.ElementTree as ET
+
 Base = declarative_base()
 
 
@@ -28,10 +31,23 @@ def create_database():
         pass
 
     engine = sqlalchemy.create_engine(f'sqlite:///{file_name}')
+    print(f'Created file {file_name}')
 
     Base.metadata.create_all(engine)
 
     return engine
+
+
+def dacco_file_to_db(file_path, db_session):
+    tree = ET.parse(file_path)
+
+    for element in tree.findall('.//Entry'):
+        for translation in element.findall('.//translation'):
+            entry = Entry(word=element.text, translation=translation.text)
+            db_session.add(entry)
+
+    db_session.commit()
+    print(f'file {file_path} inserted into {db_session.bind.url}')
 
 
 def main():
@@ -43,6 +59,8 @@ def main():
     entry = Entry(word='núvol', translation='cloud')
     session.add(entry)
     session.commit()
+
+    dacco_file_to_db('/usr/share/dacco-common/dictionaries/cateng/a.dic', session)
 
 
 if __name__ == '__main__':
